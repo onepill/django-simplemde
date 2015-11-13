8112856b013b41c3cbe1d24997957313f6853267
Siyuan Zhang <siyuan.zhang@funplus.com>
2015-11-13 11:07:34 +0800

    added setup.py  and MANIFEST.in for packaging

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-simplemde',
+    version='0.0.3',
+    description='django-simplemde is a WYSIWYG markdown editor for Django',
+    long_description=readme,
+    author="Siyuan Zhang",
+    author_email='onepill@gmail.com',
+    url='https://github.com/onepill/django-simplemde',
+    license='MIT',
+    packages=['simplemde'],
+    include_package_data=True,
+    install_requires=['setuptools'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    keywords='django,admin,wysiwyg,markdown,editor,simplemde',
+)
